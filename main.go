290c45d5814d69262534d9772e8047bb90d01eae
Stefan Crummenerl <crummenerls@gmail.com>
2021-06-13 11:35:57 +0200

    v1

--- main.go
+++ main.go
@@ -5,7 +5,22 @@ import (
 	"net/http"
 )
 
+// InMemoryPlayerStore collects data about players in memory.
+type InMemoryPlayerStore struct{}
+
+// GetPlayerScore retrieves scores for a given player.
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	handler := http.HandlerFunc(PlayerServer)
-	log.Fatal(http.ListenAndServe(":5000", handler))
+	// server is a reference to PlayerServer and takes a reference to
+	// InMemoryPlayerStore.
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+
+	// ListenAndServe listens on the TCP network address addr and then calls
+	// Serve with handler to handle requests on incoming connections. Accepted
+	// connections are configured to enable TCP keep-alives.
+	// The handler is typically nil, in which case the DefaultServeMux is used.
+	log.Fatal(http.ListenAndServe(":5000", server))
 }
--- server.go
+++ server.go
@@ -3,9 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-// PlayerServer currently returns Hello, world given _any_ request.
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+// PlayerStore stores score information about players.
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+// PlayerServer is a HTTP interface for player information.
+type PlayerServer struct {
+	store PlayerStore
+}
+
+// Implement ServerHTTP method on PlayerServer.
+// A Handler responds to an HTTP request. ServeHTTP should write reply headers
+// and data to the ResponseWriter and then return. Returning signals that the
+// request is finished; it is not valid to use the ResponseWriter or read from
+// the Request.Body after or concurrently with the completion of the ServeHTTP
+// call.
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	// return string without the provided leading prefix string.
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	// Store and methode injected during test.
+	score := p.store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, score)
 }
